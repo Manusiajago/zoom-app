0bfc9f011a9548e26a285ee0d1fab56612eecd3d
egal <egalassegaf@gmail.com>
2024-09-25 01:06:45 +0700

    update HomeCard V1

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import MeetingTypeList from '@/components/MeetingTypeList';
+
 
 const Home = () => {
   const now = new Date();
@@ -24,6 +25,8 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <MeetingTypeList />
     </section> // update 2
   )
 }
